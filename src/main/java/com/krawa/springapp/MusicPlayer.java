b3d4799e93a30dcd2c2e27451be403c08029e7c6
krawacoder <krawacoder@gmil.com>
2021-10-01 15:46:59 +0300

    updated with annotation @Autowired

--- src/main/java/com/krawa/springapp/MusicPlayer.java
+++ src/main/java/com/krawa/springapp/MusicPlayer.java
@@ -2,18 +2,27 @@ package com.krawa.springapp;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-  private List<Music> musicList = new ArrayList<>();
+
+  //  private List<Music> musicList = new ArrayList<>();
+  private ClassicalMusic classicalMusic;
+  private RockMusic rockMusic;
+  private JazzMusic jazzMusic;
 
   private String name;
   private int volume;
 
-//  public MusicPlayer(List<Music> music) {
-//    this.music = music;
-//  }
-
-  public MusicPlayer(){}
+  @Autowired
+  public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic,
+      JazzMusic jazzMusic) {
+    this.classicalMusic = classicalMusic;
+    this.rockMusic = rockMusic;
+    this.jazzMusic = jazzMusic;
+  }
 
   public String getName() {
     return name;
@@ -31,17 +40,22 @@ public class MusicPlayer {
     this.volume = volume;
   }
 
-  public List<Music> getMusicList() {
-    return musicList;
-  }
-
-  public void setMusicList(List<Music> musicList) {
-    this.musicList = musicList;
-  }
+//  public List<Music> getMusicList() {
+//    return musicList;
+//  }
+//
+//  public void setMusicList(List<Music> musicList) {
+//    this.musicList = musicList;
+//  }
 
   public void playMusic() {
-    for (Music song : musicList) {
-      System.out.println("Playing: " + song.getSong());
-    }
+    System.out.println(classicalMusic.getSong());
+    System.out.println(rockMusic.getSong());
+    System.out.println(jazzMusic.getSong());
   }
+//  public void playMusic() {
+//    for (Music song : musicList) {
+//      System.out.println("Playing: " + song.getSong());
+//    }
+//  }
 }
